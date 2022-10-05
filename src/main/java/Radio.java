ef5e5ce70731c607dd14a2ff87290fb6e3874502
Александр Масляков <lapostov@yandex.ru>
2022-10-05 13:45:02 +0400

    Change files for commit

--- src/main/java/Radio.java
+++ src/main/java/Radio.java
@@ -1,88 +1,57 @@
 public class Radio {
-    public int currentVolume; // Может устанавливать громкость
+    private int currentStation;
+    private int currentVolume;
 
-    public void setToMaxVolume() {  // Устанавливает максимальную громкость
-        currentVolume = 10;
+    public Radio(){
+
     }
 
-    public void increaseVolume() { //  Устанавливает громкость на 1 больше
-        currentVolume = 6;
-        if (currentVolume < 10) {
-            currentVolume = currentVolume + 1;
-        }
+    public Radio(int stationsCount){
+
     }
 
-    public void reduceVolume() { //  Устанавливает громкость на 1 меньше
-        currentVolume = 10;
-        if (currentVolume <= 10) {
-            currentVolume = currentVolume - 1;
-        }
-    }
-
-    public int setAddAtMax() { //Устанавливает максимальную громкость
-        int[] volumes = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        int maxVolume = 0;
-        for (int volume : volumes) {
-            if (volume >= volumes[maxVolume]) {
-                maxVolume = volume;
-            }
-        }
-        return maxVolume;
-    }
-
-    public int setAddAtMin() { //Устанавливает минимальную громкость
-        int[] volumes = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        int minVolume = 0;
-        for (int volume : volumes) {
-            if (volume <= volumes[minVolume]) {
-                volume = minVolume;
-            }
-        }
-        return minVolume;
-    }
-
-
-    public int currentStation; // Может устанавливать радиостанцию
-
-    public void setToMaxStation() {  // Устанавливает максимальную станцию
-
-        currentStation = 9;
-    }
-
-    public void next() { //  Устанавливает станцию на 1 больше
-
-        if (currentStation < 9) {
-            currentStation = currentStation + 1;
-            return;
-        }
-        if (currentStation == 9) {
-            currentStation = 0;
-        }
-    }
-
-    public void reduceStation() { //  Устанавливает станцию на 1 меньше
-
-        if (currentStation == 0) {
-            currentStation = 9;
-            return;
+    public void next(){
+        if (currentStation < 9){
+            currentStation = currentStation +1;
         } else {
             currentStation = 0;
         }
-        currentStation = currentStation - 1;
     }
+
+    public void prev(){
+        if (currentStation > 0){
+            currentStation = currentStation -1;
+        } else {
+            currentStation = 9;
+        }
+    }
+
 
     public int getCurrentStation() {
         return currentStation;
     }
 
-    public void setCurrentStation(int newCurrentStation) {
-        if (newCurrentStation < 0) {
+    public void setCurrentStation(int currentStation) {
+        if (currentStation < 0) {
             return;
         }
-        if (newCurrentStation > 9) {
+        if (currentStation > 9) {
             return;
         }
-        currentStation = newCurrentStation;
+        this.currentStation = currentStation;
     }
 
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int currentVolume) {
+        if (currentVolume < 0){
+            return;
+        }
+        if (currentVolume > 100){
+            return;
+        }
+        this.currentVolume = currentVolume;
+    }
 }
--- src/test/java/StationTest.java
+++ src/test/java/StationTest.java
@@ -3,6 +3,29 @@ import org.junit.jupiter.api.Assertions;
 
 public class StationTest {
 
+    @Test // Устанавливает станцию на 1 больше
+    public void increaseStation() {
+        Radio rad = new Radio();
+        rad.setCurrentStation(9);
+        rad.next();
+        int expected = 0;
+        int actual = rad.getCurrentStation();
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     @Test // Может устанавливать станцию
     public void ShouldSetStation() {
         Radio rad = new Radio();
@@ -22,15 +45,6 @@ public class StationTest {
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test // Устанавливает станцию на 1 больше
-    public void increaseStation() {
-        Radio rad = new Radio();
-        rad.setCurrentStation(8);
-        rad.next();
-        int expected = 9;
-        int actual = rad.getCurrentStation();
-        Assertions.assertEquals(expected, actual);
-    }
 
     @Test // Устанавливает станцию на 1 меньше
     public void reduceStation() {
